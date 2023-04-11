4b88fdd2d7a3fb9e0143d9570ece1a42a018ce30
TAEHYUN LEE <yeourain@gmail.com>
2023-04-11 21:31:46 +0900

    JS: navbar
    Make navbar transparent when it is on the top

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  if (window.scrollY > navbarHeight) {
+  console.log(window.scrollY);
+  console.log(`navbarHeight: ${navbarHeight}`);
+  if(window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
-});
-
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link == null) {
-    return;
-  }
-  navbarMenu.classList.remove('open');
-  scrollIntoView(link);
-});
-
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', () => {
-  navbarMenu.classList.toggle('open');
 });
